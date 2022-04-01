cb8ec187362077e1ae7afa84109ad574e0828d65
Akhilash <akrishn2@wm.com>
2022-04-01 11:41:11 +0530

    First commit

---
+++ eclipse-workspace/resteasy-hello-world-example/src/main/java/net/javaguides/resteasytutorial/model/HelloWorld.java
@@ -0,0 +1,23 @@
+package net.javaguides.resteasytutorial.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class HelloWorld {
+    private String message;
+
+    public HelloWorld() {}
+
+    public HelloWorld(String message) {
+        super();
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
